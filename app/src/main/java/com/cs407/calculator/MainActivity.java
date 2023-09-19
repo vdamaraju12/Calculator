4a2b89d20905f02dde51aa9fad8c49a995b83f39
vdamaraju12 <vdamaraju@wisc.edu>
2023-09-19 18:39:58 -0500

    anti-rahul

--- app/src/main/java/com/cs407/calculator/MainActivity.java
+++ app/src/main/java/com/cs407/calculator/MainActivity.java
@@ -2,7 +2,10 @@ package com.cs407.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        //goToActivity(myTextField.getText().toString());
+    }
+
+    public void goToActivity(String s) {
+        Intent intent = new Intent(this, Calculator2.class);
+        intent.putExtra("message", s);
+        startActivity(intent);
     }
 }
